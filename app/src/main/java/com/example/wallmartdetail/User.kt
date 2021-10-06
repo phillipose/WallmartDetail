4912807494ce017d7edd92171b35abe9026c01ba
phillipose <76812337+phillipose@users.noreply.github.com>
2021-10-05 19:22:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/wallmartdetail/User.kt
@@ -0,0 +1,10 @@
+package com.example.wallmartdetail
+import java.io.Serializable;
+
+class User(var fname: String ="" ,
+           var lname: String ="" ,
+           var uname: String ="" ,
+           var pword: String ="" )
+    : Serializable {
+
+}
